09560a5bffe01820ccbeb65628a41dfc5e5c2227
Keenan Ganz <38630604+s-kganz@users.noreply.github.com>
2018-07-23 11:21:46 -0700

    update gitignore, make request, catch bad filetypes

--- scripts/whaca.py
+++ scripts/whaca.py
@@ -1,6 +1,7 @@
 # file io
 from scipy.io import wavfile as wav
 from obspy import read
+import requests
 # filter narrowband sound
 from scipy.signal import medfilt
 # specgram function
@@ -8,8 +9,6 @@ from matplotlib import mlab
 import numpy as np
 
 class whaca:
-
-    DATA_TYPES = ["wav","mseed"]
     
     def __init__(self, db_thresh = 10, time_thresh = 0.5, width_thresh = None, NFFT = 512, window = None):
         # save threshold parameters
@@ -21,14 +20,34 @@ class whaca:
         self.window = window
     
     def open_wav(self,f):
+        # call scipy module
         self.rate,self.data = wav.read(f)
-    
+            
     def open_mseed(self,f):
         # TODO add support for multiple streams
         sound = read(f)[0]
         self.data = (sound.normalize() * (2 ** 31 - 1)).astype("int32")
         self.rate = sound.stats.sampling_rate
         
+    def open_url(self,url):
+    
+        DATA_TYPES = {"wav":self.open_wav,
+                  "mseed":self.open_mseed}
+        
+        # find last occurrence of dot
+        di = url.rfind(".")
+        # check the dot was found
+        if di != -1:
+            # determine file extension
+            ext = url[di + 1:]
+            # check if supported
+            if ext in DATA_TYPES.keys():
+                DATA_TYPES[ext](requests.get(url).content)
+            else:
+                raise ValueError(ext + " is not a valid file type!")
+        else:
+            raise ValueError(url + " does not look like a valid url!")   
+            
     def gen_spectro(self, process = True):
         s,f,t = mlab.specgram(self.data,
                              NFFT = self.NFFT,
@@ -37,6 +56,7 @@ class whaca:
         s = 10 * np.log10(s)
         s = np.flipud(s)
         return s,f,t
+        
     def find_sounds(self):
         # stub
         pass
